a93e75a07a3e675f25c7f473ff021e0af50966b9
ChinoManda <francofedullo22@gmail.com>
2023-09-29 12:00:58 -0300

    migracion revision

---
+++ database/migrations/2023_09_29_145821_create_revision_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRevisionTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('revision', function (Blueprint $table) {
+            $table->id();
+            $table->string("detalle");
+            $table->unsignedBigInteger("tarea_id");
+            $table->foreign("tarea_id")->references("id")->on("tarea");
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('revision');
+    }
+}
