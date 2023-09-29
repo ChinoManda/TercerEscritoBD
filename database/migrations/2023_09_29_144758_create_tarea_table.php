767567580484a0e8246b361d8fbf51faf2dbad0b
ChinoManda <francofedullo22@gmail.com>
2023-09-29 11:48:23 -0300

    make migration y model tarea

--- database/migrations/2023_09_29_144758_create_tarea_table.php
+++ database/migrations/2023_09_29_144758_create_tarea_table.php
@@ -4,20 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsuarioTable extends Migration
+class CreateTareaTable extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
-        Schema::create('usuario', function (Blueprint $table) {
+        Schema::create('tarea', function (Blueprint $table) {
             $table->id();
-            $table->string("Nombre");
-            $table->string("NombreCompleto");            
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
-        Schema::dropIfExists('usuario');
+        Schema::dropIfExists('tarea');
     }
 }
