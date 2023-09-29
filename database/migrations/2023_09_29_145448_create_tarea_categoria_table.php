c996acb21bf519b58d04ffa598df5010e9d45c95
ChinoManda <francofedullo22@gmail.com>
2023-09-29 11:57:00 -0300

    migracion tarea categoria

---
+++ database/migrations/2023_09_29_145448_create_tarea_categoria_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTareaCategoriaTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('tarea_categoria', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("tarea_id");
+            $table->foreign("tarea_id")->references("id")->on("tarea");
+            $table->string("categoria");
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tarea_categoria');
+    }
+}
